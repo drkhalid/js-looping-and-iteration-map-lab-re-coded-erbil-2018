1860c672e79ac2b649e9a99cb972da28e9e6946d
khalid jamal <kh.jamal95@gmail.com>
2018-08-12 19:09:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ function lowerCaseDrivers(list){
   return list.map((name)=> name.toLowerCase())
 }
 
-function attributesToPhrase(){
-  
+function attributesToPhrase(list){
+  let myObj ={}
+  list.map((fullName)=>firstName.assign())
+ 
+ return 
 }
