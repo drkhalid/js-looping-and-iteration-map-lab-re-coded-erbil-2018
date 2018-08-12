7cafdd471feb541125c87ea821716bda68f5ee83
khalid jamal <kh.jamal95@gmail.com>
2018-08-12 19:17:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,12 +6,8 @@ function lowerCaseDrivers(list){
 function attributesToPhrase(list){
   
   return list.map(function(fullName){
-    let myObj ={};
-    myObj.firstName.assign(fullName.split(' ')[0]);
-    myObj.lastName.assign(fullName.split(' ')[1])
-    return myObj;
-    })
-  
+    let first = fullName.split(' ')
+    let second = fullName.split(' ')
+    return {firstName: first,lastName:second}
  
- 
-}
+})}
