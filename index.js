7fafab3076d069e099359751fa4ca3f64bf24fcf
khalid jamal <kh.jamal95@gmail.com>
2018-08-12 19:21:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ function lowerCaseDrivers(list){
 
 function attributesToPhrase(list){
   return list.map(function (driver){
-    const first = driver.split(' ')[0];
-    const second = driver.split(' ')[1];
-    return {firstName: first, lastName:second}
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
+    return {firstName: driverFirst, lastName:driverLast}
  
-})}
+});
+  }
+function nameToAttributes (list) {
+   return list.map(function (driver) {
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
 
+    return { firstName: driverFirst, lastName: driverLast };
+  });
+}
