5b1b1e9ab3495df2f025a0d80c0658fd8f7c302d
khalid jamal <kh.jamal95@gmail.com>
2018-08-12 19:19:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ function lowerCaseDrivers(list){
 }
 
 function attributesToPhrase(list){
-  
-  return list.map(function(fullName){
-    const first = fullName.split(' ')[0];
-    const second = fullName.split(' ')[1];
+  return list.map(function (driver){
+    const first = driver.split(' ')[0];
+    const second = driver.split(' ')[1];
     return {firstName: first,lastName:second}
  
 })}
+
+function nameToAttributes (list) {
+  return list.map(function (driver) {
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
+
+    return { firstName: driverFirst, lastName: driverLast };
+  });
+}
